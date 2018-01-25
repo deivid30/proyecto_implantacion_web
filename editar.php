7c32f12b97320eb3da842d9936443a335b7d2830
deivid30 <deivid_n1@hotmail.es>
2018-01-25 20:18:18 +0100

    actualizar
    las mejoras son que la actualizacion de la base de datos funciona

---
+++ editar.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Editar</title>
+<meta charset="utf-8">
+</head>
+
+<body>
+<h1>A quién desear buscar</h1>
+<form name="Formulario" action="consulta_editar.php" method="post">
+          Buscar: <input type="text" name="usuario" required><br>
+          <br>
+          <input type="submit" name="guardar" value="guardar">
+          <input type="reset" name="borrar" value="borrar">          
+        
+          
+          
+          
+
+       </form>
+
+</body>
+</html>
